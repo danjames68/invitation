2e97851d47e226de11139c8de7a06b87a04e0021
Dan <danjames68@me.com>
2016-04-11 21:12:32 +0100

    Remove Wishlist Controller Code

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -1,15 +1,3 @@
 class WishlistController < ApplicationController
-  def new
-    if cookies[:wishlist]
-      cookies[:wishlist].split(",") : []
-      
-      @wishlist = cookies[:wishlist]
-      @wishlist << params[:villa_id]
-      cookies[:wishlist] = @wishlist
-    else
-      @wishlist = [params[:villa_id]] 
-      cookies[:wishlist] = @wishlist
-     end
-      redirect_to villa_path(params[:villa_id])
-  end
+  
 end
